81a7ab411096b458fa991575db28587ca07dcf7a
abhay <abhaypokhriyal2@gmail.com>
2024-06-09 00:02:44 +0530

    vercel confif fixed

--- constants/config.js
+++ constants/config.js
@@ -1,4 +1,4 @@
-export const corsOptions = {
+ const corsOptions = {
   origin: 'https://chat-fronted-34wdp7duf-abhays-projects-b84a5007.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
